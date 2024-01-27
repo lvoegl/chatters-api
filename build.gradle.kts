eee71e995a564bf09f9953356727f1c86084a91d
Lukas Vögl <lukas@voegl.org>
2024-01-28 00:06:45 +0100

    build(deps): Bump org.postgresql:postgresql from 42.6.0 to 42.7.1
    Signed-off-by: Lukas Vögl <lukas@voegl.org>

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-devtools:3.1.2")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
   implementation("org.modelmapper:modelmapper:3.2.0")
-  implementation("org.postgresql:postgresql:42.6.0")
+  implementation("org.postgresql:postgresql:42.7.1")
   implementation("com.github.twitch4j:twitch4j:1.19.0")
 
   testImplementation("com.google.truth:truth:1.3.0")
